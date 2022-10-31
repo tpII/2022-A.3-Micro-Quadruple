64c8d1a95f65042631bebed102fbc58bce4feba4
reinosofrancisco <reinosofrancisco99@gmail.com>
2022-10-30 23:22:01 -0300

    InverseKinematics for WiFi Connection

---
+++ doggov1/src/InverseKinematicsLibrary.cpp
@@ -0,0 +1,50 @@
+#include "InverseKinematicsLibrary.h"
+
+namespace InverseKinematicsLibrary
+{
+    std::array<double, 3> IK::getServosAngles(int x, int y, int z)
+    {
+        /* Declare Array with 3 angles*/
+        std::array<double, 3> angles;
+
+        double t1;
+
+        if ((x >= 0) && (y >= 0))
+        {
+            t1 = atan(y / x);
+        }
+        else if ((x <= 0) && (y <= 0))
+        {
+            t1 = M_PI + atan(y / x);
+        }
+        else if ((x > 0) && (y < 0))
+        {
+            t1 = 2 * M_PI - atan(y / x);
+        }
+        else
+            t1 = M_PI - atan(y / x);
+
+        
+        double r = sqrt(((x * x) + (y * y)) + ((z - L1) * (z - L1)));
+
+        double t3 = acos((-(L2 * L2) - (L3 * L3) + (r * r)) / (2 * L2 * L3));
+
+        double b = acos(((r * r) + (L2 * L2) - (L3 * L3)) / (2 * L2 * r));
+
+        double t2 = atan((z - L1) / (sqrt((x * x) + (y * y)))) - b;
+
+        /* Conversion to Degrees */
+        angles[0] = t1 * 180.0 / M_PI;;
+        angles[1] = t2 * 180.0 / M_PI;;
+        angles[2] = t3 * 180.0 / M_PI;;
+
+        /* Angulos en el Primer cuadrante*/
+        if (angles[0] > 359) { angles[0]-= 360;}
+        if (angles[1] > 359) { angles[1]-= 360;}
+        if (angles[2] > 359) { angles[2]-= 360;}
+
+        /* Returns the Angles in an Array */
+        return angles;
+    }
+
+}
