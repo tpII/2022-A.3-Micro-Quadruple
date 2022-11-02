b2a296b7ee450c130b5f143e845a0f465ba726c6
reinosofrancisco <reinosofrancisco99@gmail.com>
2022-11-02 01:47:00 -0300

    Wifi changes

--- doggov1/src/config.h
+++ doggov1/src/config.h
@@ -1,5 +1,7 @@
+/* Password MUST BE 8 or more characters long. */
+
 const char* ssid = "ManchitasV1";
-const char* password = "password";
+const char* password = "eshsmanchitas";
 const char* hostname = "ESP8266_1";
 
 IPAddress ip(192, 168, 1, 200);
