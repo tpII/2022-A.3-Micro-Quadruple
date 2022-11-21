312d5ccfb7c440f6323cdec50ccf7cef32e8cd67
Jeronimo Alfonsin <jeronimo.a@hyros.com>
2022-11-21 13:11:02 -0300

    Fixing Toast style issues

--- front-end-micro-quadruple/src/components/works/Works.jsx
+++ front-end-micro-quadruple/src/components/works/Works.jsx
@@ -2,6 +2,9 @@ import { RobotService } from "../services/RobotService";
 import { useRef } from "react";
 import "./works.scss"
 import { Toast } from 'primereact/toast';
+import 'primereact/resources/themes/saga-green/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
 export default function Works() {
     const robotService = new RobotService();
@@ -25,14 +28,14 @@ export default function Works() {
 
 
     return <div className="works" id="works">
+            <Toast ref={toast} position={"top-center"}/>
             {/* ADD FUNCTIONALITY TO BUTTOMS */}
             <div className="left">
                 <div className="wrapper">
-                <Toast ref={toast} />
                     <button onClick={onClick}  className="myButton"> Caminar</button>
-                    <h2>. Add Functionality</h2>
-                    <h2>.</h2>
-                    <button onClick={onClick}  className="myButton">> Agachar</button>
+                    <br />
+                    <br />
+                    <button onClick={onClick}  className="myButton"> Agachar</button>
                     {/* <h3>| 2022 - UNLP | </h3> */}
                 </div>
 
@@ -40,10 +43,9 @@ export default function Works() {
             </div>
             <div className="right">
                 <div className="wrapper">
-                    <Toast ref={toast} />
                     <button onClick={onClick}  className="myButton"> Levantar</button>
-                    <h2>.</h2>
-                    <h2>.</h2>
+                    <br />
+                    <br />
                     <button onClick={onClick} class="myButton"> Inclinar</button>
                     {/* <h3>| 2022 - UNLP | </h3> */}
                 </div>
